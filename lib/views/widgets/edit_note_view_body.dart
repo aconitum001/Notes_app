d391b0a545514cf96ebb8a2c0b0fe1c5c639d6f7
aconitum001 <skandern0000@gmail.com>
2024-05-23 20:23:45 +0100

    fixed error

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -7,7 +7,9 @@ class EditNoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [
+        CustomAppBar(),
+      ],
     );
   }
 }
